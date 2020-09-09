37104eaedecb293bd9b078e9f4fa844c3dbf0aa6
Benjamin Balazs <benjaminbalazs@gmail.com>
2020-09-10 00:55:13 +0200

    auth

--- addon/services/firebase.js
+++ addon/services/firebase.js
@@ -1,5 +1,6 @@
 // @ts-ignore Node.js issues
 import _firebase from 'firebase/app';
+import { auth } from 'firebase';
 import Service from '@ember/service';
 const firebase = _firebase;
 export default class FirebaseService extends Service {
@@ -7,6 +8,7 @@ export default class FirebaseService extends Service {
         super(...arguments);
         this.app = (name) => firebase.app(name);
         this.apps = firebase.apps;
+        this.OAuthProvider = auth.OAuthProvider;
         this.initializeApp = (options, nameOrConfig) => firebase.initializeApp(options, nameOrConfig);
     }
 }
